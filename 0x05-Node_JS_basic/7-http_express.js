0971aeed2b4affd65c2384b628242895953eab1c
SaharMFR <sweetysahora@gmail.com>
2024-07-26 23:30:50 +0000

    Update in task-7 0x05

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+const port = 1245;
+
 app.get('/', (req, res) => res.send('Hello Holberton School!'));
+
 app.get('/students', async (req, res) => {
   const title = 'This is the list of our students\n';
+
+  // Capture console.log output
+  const originalConsoleLog = console.log;
+  const logOutput = [];
+  console.log = (...args) => {
+    logOutput.push(args.join(' '));
+    originalConsoleLog.apply(console, args);
+  };
+
   try {
-    const data = await countStudents(process.argv[2]);
-    res.send(`${title}${data.join('\n')}`);
+    await countStudents(process.argv[2]);
+    res.send(`${title}${logOutput.join('\n')}`);
   } catch (error) {
     res.send(`${title}${error.message}`);
+  } finally {
+    // Restore original console.log function
+    console.log = originalConsoleLog;
   }
 });
-app.listen(1245);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
 module.exports = app;
