86a46d53eeeccdf5bc66c25219eac34d5afb5c7a
SaharMFR <sweetysahora@gmail.com>
2024-07-26 02:03:00 +0000

    Task-4 0x06

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,28 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  let stub, consoleSpy;
+
+  beforeEach(function() {
+    stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    stub.restore();
+    consoleSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with SUM, 100, 20', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+  });
+
+  it('should log The total is: 10', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+  });
+});
